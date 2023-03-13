d97b4a0fd7f2ab8e3a80b88fed586b796616353b
yqstart <yqstart@outlook.com>
2023-03-13 10:39:56 +0800

    camera: 安装ui库

--- camera/src/main/index.ts
+++ camera/src/main/index.ts
@@ -6,8 +6,9 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 300,
+    height: 300,
+    alwaysOnTop: true,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
